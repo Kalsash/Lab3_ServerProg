aa386afe702819212f977d4292be26732f7b8087
Aleksandr Kalinin <akalinin@sfedu.ru>
2025-03-13 14:18:08 +0300

    lab3_added logs

--- Lab3_ServerProg/Program.cs
+++ Lab3_ServerProg/Program.cs
@@ -1,4 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+// Настройка Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()  // Поставил минимальный уровень логгирования
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog(); //Serilog для логгирования
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -13,6 +24,14 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Middleware для логирования запросов
+app.Use(async (context, next) =>
+{
+    Log.Information("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+    await next.Invoke(); // Переход к следующему middleware
+    Log.Information("Response: {StatusCode} для {Method} {Path}", context.Response.StatusCode, context.Request.Method, context.Request.Path);
+});
 
 app.MapStaticAssets();
 app.MapRazorPages()
