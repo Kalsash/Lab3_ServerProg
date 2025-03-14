25928255ad0d46d9a1634fcba1c8c98385d45a48
Aleksandr Kalinin <akalinin@sfedu.ru>
2025-03-14 17:12:08 +0300

    lab4_post_email

--- Lab3_ServerProg/Pages/Index.cshtml.cs
+++ Lab3_ServerProg/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Lab3_ServerProg.Pages
 {
@@ -7,14 +11,52 @@ namespace Lab3_ServerProg.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        [BindProperty]
+        public SubscribeModel Subscribe { get; set; }
+
+        public bool ShowSuccessPopup { get; private set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        public async Task<IActionResult> OnPostSubscribeAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                Log.Information("New subscription from {Email}", Subscribe.Email);
+                await SendEmailAsync(Subscribe.Email);
+                ShowSuccessPopup = true; 
+                return Page(); 
+            }
+            return Page(); 
+        }
+
+        private async Task SendEmailAsync(string email)
+        {
+            string myEmail = "kalsashs@gmail.com";
+            string myPassword = System.IO.File.ReadAllText("PostPassword.txt").Trim();
+            using (var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(myEmail, myPassword),
+                EnableSsl = true,
+            })
+            {
+                var mailMessage = new MailMessage(myEmail, email, "Subscription Confirmation", "Thank you for subscribing!");
+                await smtpClient.SendMailAsync(mailMessage); 
+            }
+        }
+        public async Task<IActionResult> OnPostClosePopup()
+        {
+            ShowSuccessPopup = false;
+            return Page(); 
+        }
+
         public void OnGet()
         {
-
+            ShowSuccessPopup = false; 
         }
     }
 }
---
+++ Lab3_ServerProg/SubscribeModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3_ServerProg
+{
+    public class SubscribeModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [RegularExpression(@"^\S+@\S+\.\S+$", ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+    }
+}
