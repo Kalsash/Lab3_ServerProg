b056af66bc7d3158ee0a1da6f413ba8b78400bb0
Aleksandr Kalinin <akalinin@sfedu.ru>
2025-03-15 15:18:21 +0300

    lab4_contact

---
+++ Lab3_ServerProg/CsvHelperService.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper;
+using Lab3_ServerProg.Pages;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Lab3_ServerProg
+{
+    public static class CsvHelperService
+    {
+        private const string CsvFilePath = "contacts.csv";
+
+        public static async Task SaveRecordAsync(ContactRecord record)
+        {
+            var records = new List<ContactRecord> { record };
+            using (var writer = new StreamWriter(CsvFilePath, true))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                await csv.WriteRecordsAsync(records);
+            }
+        }
+    }
+}
--- Lab3_ServerProg/Pages/Contact.cshtml.cs
+++ Lab3_ServerProg/Pages/Contact.cshtml.cs
@@ -6,15 +6,44 @@ using System.Net;
 
 namespace Lab3_ServerProg.Pages
 {
+
     public class ContactModel : PageModel
     {
         [BindProperty]
         public SubscribeModel Subscribe { get; set; }
 
+        [BindProperty]
+        public ContactRecord ContactR{ get; set; } 
+
         public bool ShowSuccessPopup { get; private set; }
+
+        public bool ShowContactSuccess { get; private set; }
+
+
+        public async Task<IActionResult> OnPostSendAsync()
+        {
+            ModelState.Remove("Email");
+            if (!ModelState.IsValid)
+                return Page();
+
+            if (!ContactR.ContactEmail.EndsWith(".edu"))
+            {
+                ModelState.AddModelError("ContactEmail", "Email must be from a .edu domain.");
+                return Page();
+            }
+            ShowContactSuccess = true;
+            // Save data to CSV
+            await CsvHelperService.SaveRecordAsync(ContactR);
+
+            return Page();
+        }
 
         public async Task<IActionResult> OnPostSubscribeAsync()
         {
+            ModelState.Remove("ContactEmail");
+            ModelState.Remove("Name");
+            ModelState.Remove("Topic");
+            ModelState.Remove("Message");
             if (ModelState.IsValid)
             {
                 Log.Information("New subscription from {Email}", Subscribe.Email);
@@ -45,8 +74,14 @@ namespace Lab3_ServerProg.Pages
             ShowSuccessPopup = false;
             return Page();
         }
+        public IActionResult OnPostCloseContact()
+        {
+            ShowContactSuccess = false;
+            return Page();
+        }
         public void OnGet()
         {
+
         }
     }
 }
